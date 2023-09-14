e648ab561b4f32408e8493d8ab0fe5d58e7a86a4
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-14 22:41:58 +0800

    Initial Commit

---
+++ 18-swift-expenseTracker/_8_swift_expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8_swift_expenseTrackerApp.swift
+//  18-swift-expenseTracker
+//
+//  Created by Liu Ziyi on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct _8_swift_expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
