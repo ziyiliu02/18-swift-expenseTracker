9ab7ec354cce96f5cc7a057af0e14c62fcdb0219
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-15 10:54:07 +0800

    - Color Extension

---
+++ 18-swift-expenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  18-swift-expenseTracker
+//
+//  Created by Liu Ziyi on 15/9/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
