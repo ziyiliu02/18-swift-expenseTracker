2f6d62419d8f08748eb46e1744a5d11895355808
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-15 11:00:04 +0800

    - Content View

--- 18-swift-expenseTracker/ContentView.swift
+++ 18-swift-expenseTracker/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                
+            }
+            .background(Color.background)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .navigationViewStyle(.stack)
     }
 }
 
