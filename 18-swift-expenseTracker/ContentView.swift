201e83c001ac86cc8c98018eb085f8f92777bc08
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-15 11:29:32 +0800

    - Title

--- 18-swift-expenseTracker/ContentView.swift
+++ 18-swift-expenseTracker/ContentView.swift
@@ -11,7 +11,14 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                
+                VStack(alignment: .leading, spacing: 24) {
+                    // MARK: Title
+                    Text("Overview")
+                        .font(.title2)
+                        .bold()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
             }
             .background(Color.background)
             .navigationBarTitleDisplayMode(.inline)
