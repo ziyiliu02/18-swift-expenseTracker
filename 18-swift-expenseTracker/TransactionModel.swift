5aee65a4f95976f5ca0d8237888e2094d0cf1170
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-15 13:13:45 +0800

    - Transaction Model

---
+++ 18-swift-expenseTracker/TransactionModel.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionModel.swift
+//  18-swift-expenseTracker
+//
+//  Created by Liu Ziyi on 15/9/23.
+//
+
+import Foundation
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    var merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    var categoryId: Int
+    var category: String
+    let isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool 
+}
