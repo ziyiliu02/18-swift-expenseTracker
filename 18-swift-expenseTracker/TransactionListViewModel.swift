aa3cf7d461f49424fb14ae69cf496924b5276513
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-16 11:06:51 +0800

    - Setup the TransactionViewModel class to manage the call to the API

---
+++ 18-swift-expenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  TransactionListViewModel.swift
+//  18-swift-expenseTracker
+//
+//  Created by Liu Ziyi on 15/9/23.
+//
+
+import Foundation
+import Combine
+
+final class TransactionListViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getTransactions()
+    }
+    
+    func getTransactions() {
+        guard let url = URL(string: "https://designcode.io/data/transactions.json") else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    dump(response)
+                    throw URLError(.badServerResponse)
+                }
+                
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error fetching transactions", error.localizedDescription)
+                case .finished:
+                    print("Finished fetching transactions")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transactions = result
+            }
+            .store(in: &cancellables)
+
+    }
+}
--- 18-swift-expenseTracker/_8_swift_expenseTrackerApp.swift
+++ 18-swift-expenseTracker/_8_swift_expenseTrackerApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct _8_swift_expenseTrackerApp: App {
+    
+    @StateObject var transactionListVM = TransactionListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(transactionListVM)
         }
     }
 }
